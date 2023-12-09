349885a5c884ea8d07571d0bd8aaefc441b024c4
Bagus Kurniawan <bagusyuli@impstudio.co.id>
2023-12-09 22:19:43 +0700

    Add spatie backup

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Spatie\Backup\BackupServiceProvider::class,
+        App\Providers\GoogleDriveServiceProvider::class,
 
     ],
 
--- config/backup.php
+++ config/backup.php
@@ -117,6 +117,7 @@ return [
              * The disk names on which the backups will be stored.
              */
             'disks' => [
+                'google',
                 'local',
             ],
         ],
